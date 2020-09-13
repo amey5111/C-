45156ff56a1307816d7e295b91ab5f6254e056ca
amey5111 <71187037+amey5111@users.noreply.github.com>
2020-09-13 15:37:48 +0530

    Add files via upload

---
+++ Practical1.cpp
@@ -0,0 +1,14 @@
+#include <iostream.h>
+#include <conio.h>
+void main{
+    clrscr();
+    int x, y, sum;
+    cout<<"Enter your first number:";
+    cin>>x;
+    cout<<"Enter your second number:";
+    cin>>y;
+    sum= x+y;
+    cout<<"Addition of the two Numbers given by you is:";
+    cout<<sum;
+    getch();
+}

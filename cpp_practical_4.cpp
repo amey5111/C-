5ad9df439fced88a541a302ff5083b5b68206671
Amey Dilip Morye <71187037+amey5111@users.noreply.github.com>
2020-11-14 20:48:48 +0530

    Add files via upload

---
+++ cpp_practical_4.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int num;
+    cout<<"Enter a Number:";
+    cin>>num;
+    if (num%2== 0)
+    {
+        cout<<num<<" is a EVEN number";
+    }
+    else
+    {
+        cout<<num<<"is a ODD number";
+    }
+    return 0;
+
+}
